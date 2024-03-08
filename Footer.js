f1c5fd1ad84f51d9f7a5f768fec7f745f9684b6a
Canes2023 <dxc1449@miami.edu>
2024-03-07 19:07:48 -0500

    Added files needed to make portfolio work. More changes to come.

---
+++ Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <a href="https://github.com">GitHub</a>
+        <a href="https://linkedin.com">LinkedIn</a>
+        <a href="https://stackoverflow.com">Stack Overflow</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/AboutMe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <div>
+        <img src="path_to_avatar_or_photo" alt="Developer's Photo" />
+        <p>Short bio about the developer...</p>
+      </div>
+    </section>
+  );
+}
+
+export default AboutMe;
